9a45e1d9d08741c0e09f22437b96a81ee94dec2f
eliep <eprudhomme@gmail.com>
2020-09-17 11:59:25 +0200

    Add test resources

--- test/mime.js
+++ test/mime.js
@@ -5,28 +5,28 @@ const mime = require('../src/mime')
 
 describe('mime function', function () {
   it('should return \'gif\' for gif file', function () {
-    const type = mime(resolve('test/resources/zelda-head.gif'))
+    const type = mime(resolve('test/resources/zelda/zelda-head.gif'))
     expect(type).to.equal('gif')
   })
 
   it('should return \'jpg\' for jpg file', function () {
-    const type = mime(resolve('test/resources/zelda-head.jpg'))
+    const type = mime(resolve('test/resources/zelda/zelda-head.jpg'))
     expect(type).to.equal('jpg')
   })
 
   it('should return \'png\' for png file', function () {
-    const type = mime(resolve('test/resources/zelda-head.png'))
+    const type = mime(resolve('test/resources/zelda/zelda-head.png'))
     expect(type).to.equal('png')
   })
 
-  it('should return \'ansi\' for any other file type', function () {
-    const type = mime(resolve('test/resources/zelda-head.ansi'))
+  it('should return null for any other file type', function () {
+    const type = mime(resolve('test/resources/zelda/zelda-head.ansi'))
     expect(type).to.be.null
   })
 
   it('should throw an error if the file is unreadable', function () {
     expect(() => {
-      mime(resolve('test/resources/zelda-head')).to.be.an('error')
+      mime(resolve('test/resources/zelda/zelda-head'))
     }).to.throw(Error)
   })
 })
